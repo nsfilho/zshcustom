c27d8278645418999414b78789c732fba631921f
Nelio Santos <nsfilho@icloud.com>
2022-02-16 16:23:43 -0300

    feat(rpi4): adding neovim

--- nvim/after/plugin/tabnine.rc.lua
+++ nvim/after/plugin/tabnine.rc.lua
@@ -1,9 +1,12 @@
-local tabnine = require('cmp_tabnine.config')
 
-tabnine:setup({
-    max_lines = 1000;
-    max_num_results = 20;
-    sort = true;
-    run_on_every_keystroke = true;
-    snippet_placeholder = '..';
-})
+if (vim.g.arch == 'i386' or vim.g.arch == 'x86_64') then
+    local tabnine = require('cmp_tabnine.config')
+
+    tabnine:setup({
+        max_lines = 1000;
+        max_num_results = 20;
+        sort = true;
+        run_on_every_keystroke = true;
+        snippet_placeholder = '..';
+    })
+end
