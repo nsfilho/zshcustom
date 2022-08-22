4fa88c244ca9b6ff45aa2aa5205501eab73f0f37
Nelio Santos <nsfilho@icloud.com>
2022-08-22 09:59:09 -0300

    feat(neovim): removing tree-siter unused parsers

--- nvim/after/plugin/treesitter.rc.lua
+++ nvim/after/plugin/treesitter.rc.lua
@@ -1,5 +1,6 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = "all",
+    ensure_installed = { "bash", "c", "cpp", "css", "dockerfile", "gitignore", "html", "java", "javascript", "json", "lua", "make", "markdown", "rust", "scss", "toml", "typescript", "tsx", "yaml" },
+    auto_install = true,
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = true,
