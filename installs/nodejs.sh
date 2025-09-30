23527c8b4d29155d94bc2616da3c23634076d49c
Nelio <nsfilho@icloud.com>
2025-09-30 15:58:56 -0300

    feat: updating nodejs

--- installs/nodejs.sh
+++ installs/nodejs.sh
@@ -3,7 +3,10 @@ source "$HOME"/.zshcustoms/utils.sh
 checkOS
 
 if [ "$myOS" = "linux" ]; then
-    if [ "$myDistro" = "debian" ]; then
-        sudo apt install nodejs
-    fi
+    # https://nodejs.org/dist/v22.20.0/node-v22.20.0-linux-x64.tar.xz
+    wget -O /tmp/nodejs.tar.xz https://nodejs.org/dist/v22.20.0/node-v22.20.0-linux-x64.tar.xz
+    cd /usr/local && sudo tar xvf /tmp/nodejs.tar.xz
+    sudo rm -f /usr/local/node
+    sudo ln -s /usr/local/node-v22.20.0-linux-x64 /usr/local/node
+    rm -f /tmp/nodejs.tar.xz
 fi
