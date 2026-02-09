8493b57d0839f57e5ce4b956ae729c29894769ce
Nelio <nsfilho@icloud.com>
2026-02-09 08:41:50 -0300

    feat: adicionando base64 no neovim

---
+++ lazyvim/lua/plugins/endec.lua
@@ -0,0 +1,11 @@
+return {
+  "ovk/endec.nvim",
+  event = "VeryLazy",
+  opts = {
+    keymaps = {
+      defaults = false,
+      encode_base64_inplace = "gb",
+      decode_base64_inplace = "gB",
+    },
+  },
+}
