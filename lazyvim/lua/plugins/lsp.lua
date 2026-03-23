47675ac9ab039a7a4abf3ae4a5c338952cb8d143
Nelio <nsfilho@icloud.com>
2026-03-23 11:18:04 -0300

    refactor: alterando a base do workdir do lsp

--- lazyvim/lua/plugins/lsp.lua
+++ lazyvim/lua/plugins/lsp.lua
@@ -7,6 +7,16 @@ return {
         opts.inlay_hints = {}
       end
       opts.inlay_hints.enabled = false
+
+      -- Desabilitar o `.git` para determinar o root_dir do vtsls
+      -- (pois quando existe submodules, ele não le o tsconfig.json do diretorio pai -- raiz do projeto)
+      opts.servers = opts.servers or {}
+      opts.servers.vtsls = vim.tbl_deep_extend("force", opts.servers.vtsls or {}, {
+        root_dir = function(bufnr, on_dir)
+          local root = vim.fs.root(bufnr, "package.json") or vim.fn.getcwd()
+          on_dir(root)
+        end,
+      })
       return opts
     end,
   },
